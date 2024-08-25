423e268bc76f7e04ac22f299348ef508d6398a86
Mehenaaz Shaik <mehenaaz.21bce7106@vitapstudent.ac.in>
2024-08-26 00:15:12 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -8,7 +8,7 @@ CORS(app)  # Enable CORS for all routes
 def home():
     return "Welcome to the API!"
 
-@app.route('/bhfl', methods=['POST'])
+@app.route('/bfhl', methods=['POST'])
 def bfhl_post():
     try:
         # Extract data from the JSON request
