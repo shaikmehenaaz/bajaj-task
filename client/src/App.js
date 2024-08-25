9a2bdeb9c716cf19645cf43cf5498bdc3c1f5eef
Mehenaaz Shaik <mehenaaz.21bce7106@vitapstudent.ac.in>
2024-08-26 00:55:17 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ const App = () => {
   const handleSubmit = async () => {
     try {
       const parsedInput = JSON.parse(jsonInput);
-      const response = await axios.post('https://bajaj-task-rosy.vercel.app/bfhl', parsedInput);
+      const response = await axios.post('/api/bfhl', parsedInput);
       setResponseData(response.data);
     } catch (error) {
       alert('Invalid JSON or server error');
